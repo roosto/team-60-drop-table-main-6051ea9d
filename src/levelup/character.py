7d296004b127ffbd6ae392850c43cc5338ec253a
Irshad Khan <irshad.billu@gmail.com>
2023-09-21 20:43:01 +0000

    update character.py

--- src/levelup/character.py
+++ src/levelup/character.py
@@ -1,13 +1,20 @@
+from levelup.position import Position
 from levelup.map import Map
+from levelup.direction import Direction
 
 class Character:
     name = ""
-    map = None
-    current_position = None
+    map :Map = map()
+    current_position :Position = Position(-100,-100)
 
     def __init__(self, character_name):
         self.name = character_name
     
     def enter_map(self, new_map: Map):
-        self.map = new_map
+#        self.map = new_map
+        self.map = map
         self.current_position = self.map.starting_position
+
+    def move(self, direction :Direction) -> None:
+        self.current_position = self.map.calculate_new_position(
+            self.current_position, direction)
